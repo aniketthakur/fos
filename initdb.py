490ed4f13a820e6cc41e3842ad906ebc718bb25a
Prathvi Palekar <prathvi@cocoslabs.com>
2015-03-14 14:07:10 +0530

    server removing unwanted imports

--- initdb.py
+++ initdb.py
@@ -2,10 +2,10 @@
 
 from p_admin.models import  EsthenosSettings,EsthenosUser
 
-#user = EsthenosUser.create_user("admin","admin@esthenos.com",True)
-#user.add_role("ADMIN")
-#user.is_active = True
-#user.save()
+user = EsthenosUser.create_user("admin","admin@esthenos.com","Admin312",True)
+user.add_role("ADMIN")
+user.active = True
+user.save()
 
 if len(EsthenosSettings.objects.all()) ==1:
     EsthenosSettings.objects.all()[0].delete()
