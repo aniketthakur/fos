e268de384ac8515d459d31909b2eafb4d2996c9b
Prathvi Palekar <prathvi@cocoslabs.com>
2015-04-15 01:47:13 +0530

    equfax importing fixed

--- e_admin_reports/views.py
+++ e_admin_reports/views.py
@@ -5,7 +5,7 @@ from flask_login import current_user, login_required
 from e_tokens.utils import login_or_key_required
 import json,hashlib
 from e_admin.models import EsthenosUser
-from e_organisation.models import EsthenosOrgApplicationHighMarkRequest,EsthenosOrgApplicationHighMarkResponse
+from e_organisation.models import EsthenosOrgApplicationHighMarkRequest,EsthenosOrgApplicationHighMarkResponse,EsthenosOrgApplicationEqifaxResponse
 from flask import  Blueprint
 import os,tempfile
 from esthenos.crossdomain import *
@@ -15,10 +15,18 @@ from esthenos.utils import random_with_N_digits
 storage_path =  os.path.join(os.curdir,'pitaya/uploads')
 admin_reports_views = Blueprint('admin_reports_views', __name__,
     template_folder='templates')
-
+import pyexcel
 from flask.ext import excel
 from flask import Flask, make_response
 from reports.views import get_application_headers,get_application_rowdata
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 @admin_reports_views.route('/admin/reports/internal_main/download', methods=["GET"])
 @login_or_key_required
 def admin_internal_main_reports():
@@ -50,6 +58,26 @@ def admin_internal_main_reports():
         return output
 
 from e_organisation.models import EsthenosOrgApplication,EsthenosOrg
+
+
+
+@admin_reports_views.route('/admin/cbcheck/highmark/', methods=["POST"])
+@login_or_key_required
+def himark_request_reports_import():
+    c_user = current_user
+    kwargs = locals()
+    print request.files
+    if request.method == 'POST' and 'file' in request.files:
+        # handle file upload
+        filename = request.files['file'].filename
+        extension = filename.split(".")[1]
+        # Obtain the file extension and content
+        # pass a tuple instead of a file name
+        sheet = pyexcel.load_from_memory(extension, request.files['file'].read())
+        # then use it as usual
+        data = pyexcel.to_dict(sheet)
+
+    return Response(json.dumps({'status':'sucess'}), content_type="application/json", mimetype='application/json')
 
 @admin_reports_views.route('/admin/cbcheck/highmark/download', methods=["GET"])
 @login_or_key_required
@@ -187,6 +215,149 @@ def himark_request_reports():
         output.headers["Content-Disposition"] = "attachment; filename=himark_request_reports.csv"
         output.headers["Content-type"] = "text/csv"
         return output
+
+
+@admin_reports_views.route('/admin/cbcheck/equifax/', methods=["POST"])
+@login_or_key_required
+def equifax_request_reports_import():
+    c_user = current_user
+    kwargs = locals()
+    print request.files
+    if request.method == 'POST' and 'file' in request.files:
+        # handle file upload
+        filename = request.files['file'].filename
+        extension = filename.split(".")[1]
+        # Obtain the file extension and content
+        # pass a tuple instead of a file name
+        sheet = pyexcel.load_from_memory(extension, request.files['file'].read())
+        # then use it as usual
+        data = pyexcel.to_dict(sheet)
+        for k,v in data.items():
+            if k != "Series_1":
+                print k,v[0]
+                eq_resp = EsthenosOrgApplicationEqifaxResponse()
+                eq_resp.report_id=v[0]   #IntField(default=0)
+                eq_resp.reference_number=v[1]   #IntField(default=0)
+                eq_resp.unique_account_number=str(v[2])   #IntField(default=0)
+                eq_resp.date_of_issue=v[3]   #StringField(max_length=255,required=False,default="")
+                eq_resp.member_name=v[4]   #StringField(max_length=255,required=False,default="")
+                eq_resp.date_of_birth=v[5]   #StringField(max_length=255,required=False,default="")
+                eq_resp.addl_name_type1=v[6]   #StringField(max_length=255,required=False,default="")
+                eq_resp.addl_name1=v[7]   #StringField(max_length=255,required=False,default="")
+                eq_resp.addl_name_type2=v[8]   #StringField(max_length=255,required=False,default="")
+                eq_resp.addl_name2=v[9]   #StringField(max_length=255,required=False,default="")
+                eq_resp.national_id_card=v[10]   #StringField(max_length=255,required=False,default="")
+                eq_resp.passport=v[11]   #StringField(max_length=255,required=False,default="")
+                eq_resp.ration_card=v[12]   #StringField(max_length=255,required=False,default="")
+                eq_resp.voter_id=v[13]   #StringField(max_length=255,required=False,default="")
+                eq_resp.pan_card=v[14]   #StringField(max_length=255,required=False,default="")
+                eq_resp.additional_id1=v[15]   #StringField(max_length=255,required=False,default="")
+                eq_resp.additional_id2=v[16]   #StringField(max_length=255,required=False,default="")
+                eq_resp.address=v[17]   #StringField(max_length=255,required=False,default="")
+                eq_resp.state=v[18]   #StringField(max_length=255,required=False,default="")
+                eq_resp.postal=v[19]   #StringField(max_length=255,required=False,default="")
+                eq_resp.branch_id=v[20]   #StringField(max_length=255,required=False,default="")
+                eq_resp.kendra_or_centre_id=v[21]   #StringField(max_length=255,required=False,default="")
+                if not is_number(v[22]):
+                    v[22] = 0
+                eq_resp.own_mfi_indicator=v[22]   #IntField(default=0)
+                if not is_number(v[23]):
+                    v[23] = 0
+                eq_resp.total_responses=v[23]   #IntField(default=0)
+                if not is_number(v[24] ):
+                    v[24] = 0
+                eq_resp.total_responses_own=v[24]   #IntField(default=0)
+                if not is_number(v[25] ):
+                    v[25] = 0
+                eq_resp.total_responses_others=v[25]   #IntField(default=0)
+                if not is_number(v[26] ):
+                    v[26] = 0
+                eq_resp.num_of_other_mfis=v[26]   #IntField(default=0)
+                if not is_number(v[27] ):
+                    v[27] = 0
+                eq_resp.num_active_account=v[27]   #IntField(default=0)
+                if not is_number(v[28] ):
+                    v[28] = 0
+                eq_resp.num_active_account_own=v[28]   #IntField(default=0)
+                if not is_number(v[29] ):
+                    v[29] = 0
+                eq_resp.num_active_account_other=v[29]   #IntField(default=0)
+                if not is_number(v[30] ):
+                    v[30] = 0
+                eq_resp.num_closed_account=v[30]   #IntField(default=0)
+                if not is_number(v[31] ):
+                    v[31] = 0
+                eq_resp.num_closed_account_own=v[31]   #IntField(default=0)
+                if not is_number(v[32] ):
+                    v[32] = 0
+                eq_resp.num_closed_account_other=v[32]   #IntField(default=0)
+                if not is_number(v[33] ):
+                    v[33] = 0
+                eq_resp.num_past_due_account=v[33]   #IntField(default=0)
+                if not is_number(v[34] ):
+                    v[34] = 0
+                eq_resp.num_past_due_account_own=v[34]   #IntField(default=0)
+                if not is_number(v[35] ):
+                    v[35] = 0
+                eq_resp.num_past_due_account_other=v[35]   #IntField(default=0)
+                if not is_number(v[36] ):
+                    v[36] = 0
+                eq_resp.sum_current_balance=float(v[36])   #IntField(default=0)
+                if not is_number(v[37] ):
+                    v[37] = 0
+                eq_resp.sum_current_balance_own=float(v[37])   #IntField(default=0)
+                if not is_number(v[38] ):
+                    v[38] = 0
+                eq_resp.sum_current_balance_other=float(v[38])   #IntField(default=0)
+                if not is_number(v[39] ):
+                    v[39] = 0
+                eq_resp.sum_disbursed=float(v[39])   #IntField(default=0)
+                if not is_number(v[40] ):
+                    v[40] = 0
+                eq_resp.sum_disbursed_own=v[40]   #IntField(default=0)
+                if not is_number(v[41] ):
+                    v[41] = 0
+                eq_resp.sum_disbursed_other=float(v[41])   #IntField(default=0)
+                if not is_number(v[42] ):
+                    v[42] = 0
+                eq_resp.sum_installment_amount=float(v[42])   #IntField(default=0)
+                if not is_number(v[43] ):
+                    v[43] = 0
+                eq_resp.sum_installment_amount_own=v[43]   #IntField(default=0)
+                if not is_number(v[44] ):
+                    v[44] = 0
+                eq_resp.sum_installment_amount_other=float(v[44])   #IntField(default=0)
+                if not is_number(v[45] ):
+                    v[45] = 0
+                eq_resp.sum_overdue_amount=float(v[45])   #IntField(default=0)
+                if not is_number(v[46] ):
+                    v[46] = 0
+                eq_resp.sum_overdue_amount_own=float(v[46])   #IntField(default=0)
+                if not is_number(v[47] ):
+                    v[47] = 0
+                eq_resp.sum_overdue_amount_other=float(v[47])   #IntField(default=0)
+                if not is_number(v[48] ):
+                    v[48] = 0
+                eq_resp.sum_writtenoff_amount=float(v[48])   #IntField(default=0)
+                if not is_number(v[49] ):
+                    v[49] = 0
+                eq_resp.sum_writtenoff_amount_own=float(v[49])   #IntField(default=0)
+                if not is_number(v[50] ):
+                    v[50] = 0
+                eq_resp.sum_writtenoff_amount_other=float(v[50])   #IntField(default=0)
+                if not is_number(v[51] ):
+                    v[51] = 0
+                eq_resp.num_writtenoff_account=float(v[51])   #IntField(default=0)
+                if not is_number(v[52] ):
+                    v[52] = 0
+                eq_resp.num_writtenoff_account_own=float(v[52])   #IntField(default=0)
+                if not is_number(v[53] ):
+                    v[53] = 0
+                eq_resp.num_writtenoff_accountnon_own=float(v[53])   #IntField(default=0)
+                eq_resp.save()
+
+
+    return Response(json.dumps({'status':'sucess'}), content_type="application/json", mimetype='application/json')
 
 @admin_reports_views.route('/admin/cbcheck/equifax/download', methods=["GET"])
 @login_or_key_required
