5cec4502a0693bd8ba88723d023629e8268cbd6d
Prathvi Palekar <prathvi@cocoslabs.com>
2015-05-20 18:16:12 +0530

    fixed

--- e_organisation/forms.py
+++ e_organisation/forms.py
@@ -699,7 +699,7 @@ class AddApplicationMobile(Form):
             if kyc_json["kyc"][0].has_key("vtc"):
                 kyc_obj.taluk = kyc_json["kyc"][0]["vtc"]
             kyc_obj.pincode = kyc_json["kyc"][0]["pc"]
-            kyc_obj.raw = kyc_json["kyc"][0]
+            #kyc_obj.raw = kyc_json["kyc"][0]
             app.kyc_1 = kyc_obj
         if (kyc_json["kyc"][2].has_key("votercard_f") and len(kyc_json["kyc"][2]["votercard_f"])>0) or  (kyc_json["kyc"][1].has_key("pancard_f") and len(kyc_json["kyc"][1]["pancard_f"])>0):
             kyc_obj = EsthenosOrgApplicationKYC()
