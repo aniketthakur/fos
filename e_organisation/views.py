d4c0a30c511b3519c77e84cc822d7195877f0214
Prathvi Palekar <prathvi@cocoslabs.com>
2015-06-19 11:51:11 +0530

    only cgt not ready groups shown

--- e_organisation/views.py
+++ e_organisation/views.py
@@ -791,16 +791,14 @@ def get_centers_n_groups():
             groups_list.append({'id':str(grp.group_id),
                                 'group_name':str(grp.group_name)
             })
-
-        centers_list.append(
-            {'id':str(cen.center_id),
-                    'center_name':str(cen.center_name),'groups':groups_list}
-        )
     groups = EsthenosOrgGroup.objects.filter(organisation=organisation)
     for grp in groups:
-        all_group_list.append({'id':str(grp.group_id),
-                               'group_name':str(grp.group_name)
-        })
+        applications_all = EsthenosOrgApplication.objects.filter(group=group)
+        applications_cgt_ready = EsthenosOrgApplication.objects.filter(group=group,status__gte=190)
+        if(len(applications_all)>0 and len(applications_all)>len(applications_cgt_ready)):
+            all_group_list.append({'id':str(grp.group_id),
+                                   'group_name':str(grp.group_name)
+            })
 
     data = '{"centers":'+json.dumps(centers_list)+',"groups":'+json.dumps(all_group_list)+'}'
     print data
