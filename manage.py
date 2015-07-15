799aa86c4b6f1a828fc2e7d70f4ed2ce722852a8
Chaitanya Sharma <chaitanya@3bandar.org>
2015-07-16 02:47:38 +0530

    minor edits, updated esthenos.nginx.conf

--- manage.py
+++ manage.py
@@ -1,11 +1,6 @@
-__author__ = 'prathvi'
-# Set the path
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
-
+import os, sys
+from esthenos import mainapp
 from flask.ext.script import Manager, Server
-from esthenos  import mainapp
 
 manager = Manager(mainapp)
 
@@ -22,14 +17,11 @@ manager.add_command("rungunicorn", GunicornServer())
 import flask_sauth.commands
 flask_sauth.commands.add_commands( manager)
 
-#never ever copy this to pitaya/__init__.py
-#user auth
-
 from flask.ext.sauth.views import auth_views
 mainapp.register_blueprint( auth_views)
+
 from e_organisation.views import organisation_views
 mainapp.register_blueprint( organisation_views)
-
 
 from e_admin.views import admin_views
 mainapp.register_blueprint( admin_views)
@@ -37,11 +29,8 @@ mainapp.register_blueprint( admin_views)
 from e_tokens.views import token_views
 mainapp.register_blueprint( token_views)
 
-
 from reports.views import reports_views
 mainapp.register_blueprint( reports_views)
-
-
 
 from e_admin_reports.views import admin_reports_views
 mainapp.register_blueprint( admin_reports_views)
