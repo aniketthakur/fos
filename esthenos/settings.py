a919e9d8db918782a4e8df60149ac8a32d7424b6
phoenix24 <chaitanya@3bandar.org>
2015-12-29 23:06:03 +0530

    updated s3bucket keys for maitreya

--- esthenos/settings.py
+++ esthenos/settings.py
@@ -74,8 +74,8 @@ AWS_SETTINGS = {
     'AWS_CDN_PATH' : 'https://s3-ap-southeast-1.amazonaws.com/',
     'AWS_S3_BUCKET' : 'fos-maitreya-esthenos',
     'AWS_COGNITO_ID' : 'us-east-1:f662e9d4-8d9c-4b8b-adea-38ad6116de4c',
-    'AWS_ACCESS_KEY_ID' : 'AKIAITWBEHC2SAGDFQSA',
-    'AWS_SECRET_ACCESS_KEY' : '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'
+    'AWS_ACCESS_KEY_ID' : 'AKIAJUAPOKY4RDFJ6KEA',
+    'AWS_SECRET_ACCESS_KEY' : '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'
 }
 
 FEATURES = {
