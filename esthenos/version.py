f9f6b8ec716225e1457aae4cad07274e78112522
phoenix24 <chaitanya@3bandar.org>
2016-02-22 18:32:44 +0530

    version tagged: v0.3.22.02.18.32

--- esthenos/version.py
+++ esthenos/version.py
@@ -1,7 +1,7 @@
 import os, sys
 
-__VERSION__ = "0.2"
-__GIT_SHA__ = "3b7316b"
-__UPDATED__ = "22.02.18.31"
+__VERSION__ = "0.3"
+__GIT_SHA__ = "d731f2c"
+__UPDATED__ = "22.02.18.32"
 
 DEPLOY_VERSION = "%s.%s.%s" % (__VERSION__, __UPDATED__, __GIT_SHA__)
